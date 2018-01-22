79eb923ecb05e48fcb1286b3b3224df1f8123582
Brandon Villa <bornofos@gmail.com>
2018-01-22 00:41:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,13 +24,16 @@ var pie = {
 }
 
 function makeCake() {
-  var updateCakeStatus;
-  mix(updateCakeStatus)
+  var updateCakeStatus = updateStatus.bind(this);
+  updateCakeStatus("Prep")
+  mix.call(cake, updateCakeStatus)
 }
 
 function makePie() {
-  var updatePieStatus;
-  mix(updatePieStatus)
+  var updatePieStatus = updateStatus.bind(this)
+  updatePieStatus("Prep")
+  pie.decorate = cake.decorate.bind(pie)
+  mix.call(pie, updatePieStatus)
 }
 
 function updateStatus(statusText) {
@@ -40,14 +43,14 @@ function updateStatus(statusText) {
 function bake(updateFunction) {
   var status = "Baking at " + this.bakeTemp + " for " + this.bakeTime
   setTimeout(function() {
-    cool(updateFunction)
+    cool.call(this, updateFunction)
   }, 2000)
 }
 
 function mix(updateFunction) {
   var status = "Mixing " + this.ingredients.join(", ")
   setTimeout(function() {
-    bake(updateFunction)
+    bake.call(this, updateFunction)
   }, 2000)
   updateFunction(status)
 }
@@ -60,8 +63,10 @@ function cool(updateFunction) {
 }
 
 function makeDessert() {
-  //add code here to decide which make... function to call
-  //based on which link was clicked
+  if ( this.parentNode.id === 'cake' )
+    makeCake.call(this.parentNode)
+  else
+    makePie().call(this.parentNode)
 }
 
 function serve(message, customer) {
